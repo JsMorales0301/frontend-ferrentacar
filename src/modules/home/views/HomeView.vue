9f324c95b577b5e83ed5c1a3221f1d0c1650a5d5
Joan Morales <josmoralesp0307@outlook.es>
2023-03-06 20:32:30 -0500

    add unfinished first view of the landing page

---
+++ src/modules/home/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <div class="separator-2">
+        <h1 class="text-center">Welcome FerRentacar</h1>
+        <p class="text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident, magnam? Placeat omnis, esse odit laboriosam, doloremque a eius recusandae.</p>
+    </div>
+
+    <div class="vehicle-main separator-2">
+        <img src="@/assets/home/mercedesAMG.webp" alt="carro-landing" class="vehicle">
+    </div>
+
+    <div class="separator-1">
+        <FormHome />
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import FormHome from '../components/FormHome.vue';
+import '../styles/HomeView.css'
+</script>
