170ba153ceb449b66c7f08ce6bbc0d253108f300
Joan Morales <josmoralesp0307@outlook.es>
2023-03-06 20:30:38 -0500

    add navigation bar in home

---
+++ src/modules/home/components/NavHome.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="nav-home">
+        <div>
+            <a href="">HOME</a>
+        </div>
+        <div>
+            <a href="">SVG</a>
+        </div>
+        <div>
+            <a href="">LOGIN</a>
+            <span class="separated">|</span>
+            <a href="">REGISTER</a>
+        </div>
+    </nav>
+</template>
+
+<script setup lang="ts">
+import '../styles/NavHome.css'
+</script>
