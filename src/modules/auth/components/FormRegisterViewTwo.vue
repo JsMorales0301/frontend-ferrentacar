c43c4d8fe2eddc6fd9d37458ef9eb82e413a2936
Joan Morales <josmoralesp0307@outlook.es>
2023-03-11 19:57:58 -0500

    Create view for registration form

---
+++ src/modules/auth/components/FormRegisterViewTwo.vue
@@ -0,0 +1,35 @@
+<template>
+    <form>
+        
+        <div class="field">
+            <label for="">Identification number</label>
+            <input type="number" class="input-register">
+        </div>
+
+        <div class="field separator-1">
+            <label for="">Name</label>
+            <input type="text" class="input-register">
+        </div>
+
+        <div class="field separator-1">
+            <label for="">Last name</label>
+            <input type="text" class="input-register">
+        </div>
+
+        <div class="field separator-1">
+            <label for="">Phone number</label>
+            <input type="tel" class="input-register">
+        </div>
+
+        <div class="field separator-1">
+            <label for="">Birthdate</label>
+            <input type="date" class="input-register">
+        </div>
+
+        <div class="separator-2 btn-flex">
+            <button class="btn">Back</button>
+            <button class="btn">Next</button>
+        </div>
+
+    </form>
+</template>
